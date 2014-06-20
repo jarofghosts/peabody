a6c74ccf9672aecbb2076b36bf3179973e58bc81
jesse keane <me@jessekeane.me>
2014-06-19 18:14:29 -0700

    external assets working

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var http = require('http')
+  , url = require('url')
 
 var memento = require('memento-client')
   , request = require('hyperquest')
+
+var WAYBACK_HOST = 'web.archive.org'
+  , isAsset = /\/web\/\d{14}/
 
 module.exports = peabody
 
@@ -9,11 +13,23 @@ function peabody(timestamp) {
   return http.createServer(handler)
 
   function handler(req, res) {
+    if(isAsset.test(req.url)) {
+      return request.get(
+          url.format({
+              host: WAYBACK_HOST
+            , pathname: url.parse(req.url).path
+            , protocol: 'http'
+          })
+        , assetRespond
+      )
+    }
+
     memento(req.url, timestamp, checkSites)
 
     function checkSites(err, sites) {
       if(err || !sites.length) {
         res.writeHead(404, {'content-type': 'text/plain'})
+
         return res.end('Site not available')
       }
 
@@ -21,7 +37,40 @@ function peabody(timestamp) {
     }
 
     function respond(err, response) {
-      res.writeHead(200, response.headers)
+      if(err) {
+        res.writeHead(404, {'content-type': 'text/plain'})
+
+        return res.end('Site not available')
+      }
+
+      response.pipe(res)
+      res.writeHead(response.statusCode, response.headers)
+    }
+
+    function assetRespond(err, response) {
+      if(err) {
+        res.writeHead(404, {'content-type': 'text/plain'})
+
+        return res.end('Site not available')
+      }
+
+      if(response.statusCode === 302) {
+        return request.get(
+            url.format({
+                host: WAYBACK_HOST
+              , pathname: response.headers.location
+              , protocol: 'http'
+            })
+          , assetRespond
+        )
+      }
+
+      res.writeHead(response.statusCode, response.headers)
+
+      if(response.statusCode < 200 || response.statusCode > 299) {
+        res.end()
+      }
+
       response.pipe(res)
     }
   }
